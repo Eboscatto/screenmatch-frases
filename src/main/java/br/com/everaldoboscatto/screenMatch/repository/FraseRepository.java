e7278ee62a121042e08e1173e576a1c0045f1102
Eboscaatto <e_boscatto@hotmail.com>
2024-05-15 14:18:57 -0300

    Implementado o método de buscar frase de forma aleatória

--- src/main/java/br/com/everaldoboscatto/screenMatch/repository/FraseRepository.java
+++ src/main/java/br/com/everaldoboscatto/screenMatch/repository/FraseRepository.java
@@ -2,7 +2,10 @@ package br.com.everaldoboscatto.screenMatch.repository;
 
 import br.com.everaldoboscatto.screenMatch.model.Frase;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
+    Frase buscaFraseAleatoria();
 
 }
